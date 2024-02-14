b81243a169ee88c5711cf09cab2740ad1f5a9abe
mehdia8 <mehdiabid.2002@gmail.com>
2024-02-14 23:51:23 +0100

    Slices

--- exercises/slices/slices3/main.go
+++ exercises/slices/slices3/main.go
@@ -1,13 +1,13 @@
 // slices3
 // Make me compile!
 
-// I AM NOT DONE
+// I AM DONE
 package main
 
 import "fmt"
 
 func main() {
 	names := []string{"John", "Maria", "Carl", "Peter"}
-	names = append()
+	names = append(names, "Jeremy")
 	fmt.Println(names)
 }
